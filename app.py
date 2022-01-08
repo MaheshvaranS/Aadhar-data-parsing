098ee89c531e3e38a46e08c65341ecb89048b948
Maheshvaran <75715855+MaheshvaranS@users.noreply.github.com>
2022-01-09 00:01:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,6 @@ def upload_file():
 
     return render_template('index.html')
 if __name__ == '__main__':
-    app.run(port=8080, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=80, threaded=True, debug=True)
 
 
